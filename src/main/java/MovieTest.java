7c23afe862a01cbd055e45e909419e029d7aa209
Toaster996 <32509408+Toaster996@users.noreply.github.com>
2018-05-12 22:25:26 +0200

    Created Tests

---
+++ src/main/java/MovieTest.java
@@ -0,0 +1,35 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MovieTest {
+
+    public static final String MOVIE_TITLE = "Requiem for a dream";
+    private Movie movie;
+
+    @Before
+    public void setUp() {
+        movie = new Movie(MOVIE_TITLE, Movie.REGULAR);
+    }
+
+
+    @Test
+    public void getPriceCode() {
+        assertEquals(movie.getPriceCode(), Movie.REGULAR);
+    }
+
+
+    @Test
+    public void setPriceCode() {
+        movie.setPriceCode(Movie.CHILDRENS);
+        assertEquals(movie.getPriceCode(), Movie.CHILDRENS);
+    }
+
+
+    @Test
+    public void getTitle() {
+        assertEquals(movie.getTitle(), MOVIE_TITLE);
+    }
+
+}
