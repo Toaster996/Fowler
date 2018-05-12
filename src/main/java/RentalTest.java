f8b2cc8d04deb8201bcbfe6c77b2547cf85edf00
Toaster996 <32509408+Toaster996@users.noreply.github.com>
2018-05-12 23:26:25 +0200

    Replace Temp with Query II

--- src/main/java/RentalTest.java
+++ src/main/java/RentalTest.java
@@ -6,6 +6,7 @@ import static org.junit.Assert.assertEquals;
 public class RentalTest {
 
     public static final int DAYS_RENTED = 3;
+    private static final int DAYS_RENTED_ONE = 1;
 
     private Movie movie;
     private Rental rental;
@@ -24,5 +25,42 @@ public class RentalTest {
     @Test
     public void getDaysRented() {
         assertEquals(rental.getDaysRented(), DAYS_RENTED);
+    }
+
+    @Test
+    public void chargeAmountRegular(){
+        movie.setPriceCode(Movie.REGULAR);
+        double actual = rental.getCharge();
+        assertEquals(5d, actual, 0);
+    }
+
+    @Test
+    public void chargeAmountChildren(){
+        movie.setPriceCode(Movie.CHILDRENS);
+        double actual = rental.getCharge();
+        assertEquals(3d, actual, 0);
+    }
+
+    @Test
+    public void chargeAmountNewRelease(){
+        movie.setPriceCode(Movie.NEW_RELEASE);
+        double actual = rental.getCharge();
+        assertEquals(12d, actual, 0);
+    }
+
+    @Test
+    public void getFrequentRenterPointsOne(){
+        movie.setPriceCode(Movie.NEW_RELEASE);
+        rental = new Rental(movie, DAYS_RENTED_ONE);
+        int actual = rental.getFrequentRenterPoints();
+        assertEquals(1, actual);
+    }
+
+    @Test
+    public void getFrequentRenterPointsTwo(){
+        movie.setPriceCode(Movie.NEW_RELEASE);
+        rental = new Rental(movie, DAYS_RENTED);
+        int actual = rental.getFrequentRenterPoints();
+        assertEquals(2, actual);
     }
 }
